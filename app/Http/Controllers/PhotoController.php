7a8ba88a7db99eb50dca7921d6d0672e5ad61190
thiennv <vietthien211@gmail.com>
2019-08-26 09:56:43 +0700

    first

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Photo;
+use File;
+use Image;
+use Illuminate\Http\Request;
+
+class PhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $photos = Photo::all();
+        return view('photo', compact('photos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+    //upload images
+    protected $photo;
+
+    /**
+     * [__construct description]
+     * @param Photo $photo [description]
+     */
+    public function __construct(Photo $photo)
+    {
+        $this->photo = $photo;
+    }
+
+    public function uploadImage(Request $request)
+    {
+        $request->validate([
+            'image' => 'required',
+            'image.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        //check if image exist
+        if ($request->hasFile('image')) {
+            $images = $request->file('image');
+
+            //setting flag for condition
+            $org_img = $thm_img = true;
+
+            // create new directory for uploading image if doesn't exist
+            if( ! File::exists('images/originals/')) {
+                $org_img = File::makeDirectory('images/originals/', 0777, true);
+            }
+            if ( ! File::exists('images/thumbnails/')) {
+                $thm_img = File::makeDirectory('images/thumbnails', 0777, true);
+            }
+
+            // loop through each image to save and upload
+            foreach($images as $key => $image) {
+                //create new instance of Photo class
+                $newPhoto = new $this->photo;
+                //get file name of image  and concatenate with 4 random integer for unique
+                $filename = rand(1111,9999).time().'.'.$image->getClientOriginalExtension();
+                //path of image for upload
+                $org_path = 'images/originals/' . $filename;
+                $thm_path = 'images/thumbnails/' . $filename;
+
+                $newPhoto->image     = 'images/originals/'.$filename;
+                $newPhoto->thumbnail = 'images/thumbnails/'.$filename;
+
+                //don't upload file when unable to save name to database
+                if ( ! $newPhoto->save()) {
+                    return false;
+                }
+
+                // upload image to server
+                if (($org_img && $thm_img) == true) {
+                   Image::make($image)->fit(900, 500, function ($constraint) {
+                           $constraint->upsize();
+                       })->save($org_path);
+                   Image::make($image)->fit(270, 160, function ($constraint) {
+                       $constraint->upsize();
+                   })->save($thm_path);
+                }
+            }
+        }
+
+    return redirect()->action('PhotoController@index');
+
+    }
+}
